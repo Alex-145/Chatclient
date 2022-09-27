eefda06ebda635dca176c03015006e3505408b0d
Alex-145 <linoalex145@gmail.com>
2022-09-26 19:49:33 -0500

    servidor

---
+++ public/chat.js
@@ -0,0 +1,22 @@
+const socket = io()
+let message = document.getElementById('message')
+let username = document.getElementById('username')
+let btn = document.getElementById('send')
+let output = document.getElementById('output')
+let actions = document.getElementById('actions')
+
+
+btn.addEventListener('click', function () {
+    socket.emit('chat:message', {
+        username: username.value,
+        message: message.value
+    });
+
+    console.log(username.value, message.value);
+})
+
+socket.on('chat:message', function (data){
+    output.innerHTML += `<p>
+    <strong>${data.username}</strong>:${data.message}
+</p>`
+});
